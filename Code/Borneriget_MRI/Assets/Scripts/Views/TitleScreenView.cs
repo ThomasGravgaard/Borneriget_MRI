29c8b3db7475834deff1c74538d99ee4f080f567
Thomas Gravgaard <tg@sentio.net>
2021-04-06 14:28:02 +0200

    Added menu speaks and a title screen with logo.

---
+++ Code/Borneriget_MRI/Assets/Scripts/Views/TitleScreenView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Borneriget.MRI
+{
+    public class TitleScreenView : MonoBehaviour
+    {
+        [SerializeField]
+        private float showTimer = 3f;
+
+        public event Action TimerDone;
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            StartCoroutine(ViewCo());
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private IEnumerator ViewCo()
+        {
+            yield return new WaitForSeconds(showTimer);
+            TimerDone();
+        }
+    }
+}
